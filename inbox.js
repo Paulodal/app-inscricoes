d19b58f598be0372520f5213183987ac92de31cb
Ricardo Misawa <rmmisawa@gmail.com>
2021-10-11 18:48:35 -0300

    Inbox processing

--- inbox.js
+++ inbox.js
@@ -29,38 +29,51 @@ s3.listObjects(params, (err, data) => {
   var email = "";
   var telefone = "";
   var cidade = "";
+
   contents.forEach((x, i) => {
     filename = x["Key"];
-    // console.log(filename, i);
-    s3.getObject({ Key: filename, Bucket: "email-repo-pp" }, (err, data) => {
-      if (err) throw err;
 
-      body = data.Body.toString("utf-8");
+    s3.getObject(
+      {
+        Key: filename,
+        Bucket: "email-repo-pp",
+      },
+      async (err, data) => {
+        if (err) throw err;
 
-      start = body.indexOf("Nome: ") + 6;
-      end = body.indexOf("<br>", start);
-      name = cleanup(body.substring(start, end));
+        body = data.Body.toString("utf-8");
 
-      start = body.indexOf("E-mail: ") + 8;
-      end = body.indexOf("<br>", start);
-      email = cleanup(body.substring(start, end));
+        start = body.indexOf("Nome: ") + 6;
+        end = body.indexOf("<br>", start);
+        name = cleanup(body.substring(start, end));
 
-      start = body.indexOf("Telefone: ") + 10;
-      end = body.indexOf("<br>", start);
-      telefone = cleanup(body.substring(start, end));
+        start = body.indexOf("E-mail: ") + 8;
+        end = body.indexOf("<br>", start);
+        email = cleanup(body.substring(start, end));
 
-      start = body.indexOf("Cidade: ") + 8;
-      end = body.indexOf("<br>", start);
-      cidade = cleanup(body.substring(start, end));
+        start = body.indexOf("Telefone: ") + 10;
+        end = body.indexOf("<br>", start);
+        telefone = cleanup(body.substring(start, end));
 
-      console.log(name, email, telefone, cidade);
+        start = body.indexOf("Cidade: ") + 8;
+        end = body.indexOf("<br>", start);
+        cidade = cleanup(body.substring(start, end));
 
-      const volunteer = Volunteer.create({
-        cvv_number: "62720",
-        name: name,
-        email: email,
-      });
-    });
+        // console.log(name, email, telefone, cidade);
+
+        const v = await Volunteer.findOne({
+          order: [["cvv_number", "DESC"]],
+        });
+
+        console.log(v.cvv_number, name, email, telefone, cidade);
+
+        const volunteer = await Volunteer.create({
+          cvv_number: v.cvv_number + 1,
+          name: name,
+          email: email,
+        });
+      }
+    );
   });
 });
 
@@ -72,9 +85,11 @@ function cleanup(str) {
   str = str.split("=C3=A9").join("é");
   str = str.split("=C3=89").join("É");
   str = str.split("=C3=A1").join("á");
-  str = str.split("=C3=A2").join("Á");
+  str = str.split("=C3=81").join("Á");
   str = str.split("=C3=A3").join("ã");
   str = str.split("=C3=83").join("Ã");
+  str = str.split("=C3=A2").join("â");
+  str = str.split("=C3=82").join("Â");
   str = str.split("=C3=AA").join("ê");
   str = str.split("=C3=BA").join("ú");
   str = str.split("=C3=B5").join("õ");
