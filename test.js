50af88a1977ccc797266ab6a142507c19775771c
Ricardo Misawa <rmmisawa@gmail.com>
2021-10-13 07:53:04 -0300

    Read DB syncronously test

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 require("./database");
 const Volunteer = require("./models/Volunteer");
 
-async function readdb() {
-  const v = await Volunteer.findOne({
-    order: [["cvv_number", "DESC"]],
-  })
-  console.log(v.cvv_number);
-  return v;
-}
-
-for(i = 1; i <= 20; i++) {
-  const v = readdb();
-  console.log(i);
-}
+(async () => {
+  for (i = 1; i <= 20; i++) {
+    const v = await Volunteer.findOne({
+      order: [["cvv_number", "DESC"]],
+    });
+    console.log(v.cvv_number);
+    console.log(i);
+  }
+})();
